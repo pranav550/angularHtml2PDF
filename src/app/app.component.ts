2c94ef7d44976f56a10d95b2c72d8bcf99b85667
pranav550 <pranavverma550@gmail.com>
2020-03-21 11:02:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as jspdf from "jspdf";
+import html2canvas from "html2canvas";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'html2Pdf';
+
+  download() {
+    var element = document.getElementById('table')
+    html2canvas(element).then((canvas) => {
+      console.log(canvas)
+      var imgData = canvas.toDataURL('image/png')
+      var doc = new jspdf()
+      var imageHeight = canvas.height * 208 / canvas.width
+      doc.addImage(imgData, 0, 0, 208, imageHeight)
+      doc.save("image.pdf")
+    })
+  }
+
 }
